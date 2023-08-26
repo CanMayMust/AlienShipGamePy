892080113ec715b643c592908b78d323bbbad78e
DiegoGarciaChristiano <diego.garcia.christiano@gmail.com>
2023-08-26 16:44:14 +0900

    设置背景色（230，230，230）；每次循环都要重绘屏幕。创建设置类Settings类于settings.py，把AlienInvasion类中的死数字挪到Settings中统一存储

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,16 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
   """管理游戏资源和行为的类"""
   def __init__(self):
     pygame.init()
-    self.screen = pygame.display.set_mode((1200, 780))
-    pygame.display.set_caption("外星人入侵")
+    self.settings = Settings()
+    self.screen = pygame.display.set_mode(
+      (self.settings.screen_width, self.settings.screen_height))
+    pygame.display.set_caption(self.settings.win_caption)
 
   def run_game(self):
     """游戏主消息循环"""
@@ -14,6 +18,8 @@ class AlienInvasion:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           sys.exit()
+      # 每次循环都要重绘屏幕
+      self.screen.fill(self.settings.bg_color)
       pygame.display.flip()
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """存储所有设置的类"""
+    def __init__(self):
+        """初始化游戏设置"""
+        # 屏幕
+        self.screen_width = 1200
+        self.screen_height = 780
+        self.bg_color = (230, 230, 230)
+        self.win_caption = "外星人入侵"
