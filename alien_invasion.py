6d19ac3455b772ab44a896819723669df0c70feb
DiegoGarciaChristiano <diego.garcia.christiano@gmail.com>
2023-08-26 18:20:28 +0900

    添加images目录，存储项目中所用的图片；创建Ship类于ship.py；alien_invasion.py中，在屏幕上绘制飞船。

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,26 +2,35 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
-  """管理游戏资源和行为的类"""
-  def __init__(self):
-    pygame.init()
-    self.settings = Settings()
-    self.screen = pygame.display.set_mode(
-      (self.settings.screen_width, self.settings.screen_height))
-    pygame.display.set_caption(self.settings.win_caption)
+    """管理游戏资源和行为的类"""
 
-  def run_game(self):
-    """游戏主消息循环"""
-    while True:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          sys.exit()
-      # 每次循环都要重绘屏幕
-      self.screen.fill(self.settings.bg_color)
-      pygame.display.flip()
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption(self.settings.win_caption)
+        self.ship = Ship(self)  # self指向的是当前AlienInvasion实例
 
-if __name__ == '__main__':
-  ai = AlienInvasion()
-  ai.run_game()
+    def run_game(self):
+        """游戏主消息循环"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # 每次循环都要重绘屏幕
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # 让最近绘制的屏幕可见
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ ship.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Ship:
+    """管理飞船的类"""
+
+    """
+    Ship的方法__init__() 接受两个参数：引用self和指向当前AlienInvasion实例的引用。这让Ship能够访问AlienInvasion 中定义的所有游戏资源
+    """
+
+    def __init__(self, ai_game):
+        """初始化飞船，并设置其初始位置"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 加载飞船图像并获取其外接矩形
+        self.ship_image = pygame.image.load("images/ship.bmp")
+        self.rect = self.ship_image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.ship_image, self.rect)
