013fd073b38d89dcba7398a61455c02d9ebfc953
DiegoGarciaChristiano <diego.garcia.christiano@gmail.com>
2023-08-26 16:06:19 +0900

    创建主应用类和消息循环，AlienInvasion类含循环run_game()

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,5 +6,16 @@ class AlienInvasion:
   def __init__(self):
     pygame.init()
     self.screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("外星人入侵")
 
-print("Hello World!")
+  def run_game(self):
+    """游戏主消息循环"""
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          sys.exit()
+      pygame.display.flip()
+
+if __name__ == '__main__':
+  ai = AlienInvasion()
+  ai.run_game()
