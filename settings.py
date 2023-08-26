4f418629a38445cee8eef3e58240db7370c90ca5
DiegoGarciaChristiano <diego.garcia.christiano@gmail.com>
2023-08-26 21:21:48 +0900

    设定速度变量ship_speed

--- settings.py
+++ settings.py
@@ -7,6 +7,9 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 780
         self.bg_color = (230, 230, 230)  # 书例程颜色
+
         # self.bg_color = (135, 206, 250)# 练习12-1：蓝色天空背景
         # self.bg_color = (254, 192, 236)  # 练习12-2：角色背景
+
         self.win_caption = "外星人入侵"
+        self.ship_speed = 1.5  # 飞船移动速度设置
