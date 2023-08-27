199235bf7763ffca56765e2af308b2e391141ff7
DiegoGarciaChristiano <diego.garcia.christiano@gmail.com>
2023-08-27 11:34:12 +0900

    7、射击，加子弹设置Bullet类、Setting类

--- settings.py
+++ settings.py
@@ -13,3 +13,9 @@ class Settings:
 
         self.win_caption = "外星人入侵"
         self.ship_speed = 1.5  # 飞船移动速度设置
+
+        # 子弹设置
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
